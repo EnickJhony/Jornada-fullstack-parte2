0ea43df518262da7ed6fa16095a3fb9baa09a70a
Enick <enick.jhony@gmail.com>
2022-12-15 16:46:15 -0400

    Modificações no arquivo de Front

--- frontend/src/components/card/card.jsx
+++ frontend/src/components/card/card.jsx
@@ -1,9 +1,13 @@
+import "./card.css";
+
 function Card(){
     return(
-        <div className="card mb-5" >
+        <div className="card mb-3" >
           <img src="http://picsum.photos/300/200" className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <a href="#" className="card-title">
+              <h5 className="card-title"> Card title </h5>
+            </a>
             <p className="card-text">Categoria</p>
             <a href="#" className="btn btn-danger">
               Remover
