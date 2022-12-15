bacd0b5602c7dd27d5bc8daa88c815f6bdda7889
Enick <enick.jhony@gmail.com>
2022-12-15 16:45:56 -0400

    Modificações com o Prettier

--- backend/src/category/category.controller.js
+++ backend/src/category/category.controller.js
@@ -1,23 +1,23 @@
-const service = require("./category.service");
+const service = require('./category.service')
 
 const findAll = async (req, res) => {
-  const categories = await service.findAll();
-  res.send(categories);
-};
+  const categories = await service.findAll()
+  res.send(categories)
+}
 
 const create = async (req, res) => {
-  const category = req.body;
+  const category = req.body
 
   if (!category || !category.name) {
-    return res.status(400).send({ message: "Dados inválidos." });
+    return res.status(400).send({ message: 'Dados inválidos.' })
   }
 
-  const newCategory = await service.create(category);
+  const newCategory = await service.create(category)
 
-  res.status(201).send(newCategory);
-};
+  res.status(201).send(newCategory)
+}
 
 module.exports = {
   findAll,
-  create,
-};
+  create
+}
