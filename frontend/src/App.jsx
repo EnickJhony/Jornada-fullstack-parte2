3db64da09af37bd7327313b5e54d1ea83c5cfe87
Enick Jhony <enick.jhony@gmail.com>
2022-12-16 21:31:33 -0400

    Criando rotas e criando o ItemList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,23 @@
-import "./App.css";
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
-import Card from "./components/card/card"
+import './App.css'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import Card from './components/card/card'
+import { Routes, Route } from 'react-router-dom'
+import ItemList from './components/itemList/itemList'
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <div className="content container mb-3 mt-3">
-        <div className="row">
-          <div className="col-4">
-            <Card />
-          </div>
-          <div className="col-4">
-            <Card />
-          </div>
-          <div className="col-4">
-            <Card />
-          </div>
-          <div className="col-4">
-            <Card />
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<ItemList />} />
+        <Route path="/cadastro" element={'Cadastro'} />
+      </Routes>
 
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
